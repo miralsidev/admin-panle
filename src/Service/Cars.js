bf3740ac0ff5d40ec003f3da27434d03832fa6ce
miralsidev <miralsidev@gmail.com>
2024-06-03 22:59:06 +0530

    Addcode3

--- src/Service/Cars.js
+++ src/Service/Cars.js
@@ -1,17 +1,17 @@
 import axios from "axios";
-
-export const AddCarsServices = (userData) => {
+// import axiosInstance from "../Helper/AxiosInstance";
+// export const AddCarsServices =async (userData) => {
+//     return await axiosInstance.post('http://localhost:5000/cars/AddCars', userData);
+// }
+export const AddCarsServices =(userData) => {
     return axios.post('http://localhost:5000/cars/AddCars', userData);
 }
-
 export const UpdateCarsServices = (userData, carId) => {
     return axios.put(`http://localhost:5000/cars/UpdateCars/${carId}`, userData)
 }
-
 export const viewData = (carId) => {
     return axios.get(`http://localhost:5000/cars/viewData/${carId}`)
 }
-
 export const deleteData = (carId)=>{
     return axios.delete(`http://localhost:5000/cars/deleteCars/${carId}`)
 
